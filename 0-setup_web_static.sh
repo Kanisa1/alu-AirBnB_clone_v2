290e7c4cd78813b6ed22308a3eb42d28f0c42908
Kanisa1 <k.thiak@alustudent.com>
2024-06-07 13:29:52 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
- Bash script that sets up your web servers for the deployment of web_static
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
 apt-get update
 apt-get -y install nginx
@@ -6,7 +7,7 @@ apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-echo "<h1>Kanisa</h1>" > /data/web_static/releases/test/index.html
+echo "<h1>Abimbola Ronald</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
